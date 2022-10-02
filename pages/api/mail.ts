5e0f39545c39334c2ed06fb406fa24ee78c47baf
Ritik <ritik.kumar006@gmail.com>
2022-10-02 15:01:46 +0530

    Debugging

--- pages/api/mail.ts
+++ pages/api/mail.ts
@@ -11,6 +11,7 @@ type Data = {
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse<Data>) {
   const body = req.body;
+  console.log(body);
   const message = `
     Name: ${body.name}\r\n
     Email: ${body.email}\r\r
